3b6620b6545880543912a59f2e4ca3b63d7bd8e1
John Woods <john.woods@marcottelab.org>
2011-09-09 15:06:06 -0500

    Plotter refreshes automatically when plot script is edited. Handles errors in script by popping up alert box.

--- lib/sciruby/plotter.rb
+++ lib/sciruby/plotter.rb
@@ -5,19 +5,47 @@ require "rsvg2"
 module SciRuby
   class Plotter
     def initialize script_or_handle
+      update = false
       handle = begin
         if script_or_handle.is_a?(RSVG::Handle)
           script_or_handle
         else
+          update = File.mtime(script_or_handle)
           SciRuby::Plotter.create_handle File.read(script_or_handle)
         end
       end
 
       Shoes.app :title => "Plotter - SciRuby", :width => handle.width+20, :height => handle.height+20 do
+        puts app.win.inspect
+        puts app.canvas.class.inspect
         strokewidth 1
         fill white
-        rect 10, 10, handle.width+2, handle.height+2
-        image(:data => handle).move(11, 11)
+        r   = rect 10, 10, handle.width+2, handle.height+2
+        img = image(:data => handle).tap { |i| i.move(11, 11) }
+
+        # If a script was provided, watch it for updates
+        every(2) do
+          new_time = File.mtime(script_or_handle)
+          unless new_time == update
+            update  = new_time
+            begin
+              handle  = SciRuby::Plotter.create_handle(File.read(script_or_handle))
+              img.real.clear # This may create a memory leak, but img.remove does not work.
+
+              # Update window and rectangle size to accommodate new image, in case size has changed.
+              #app.resize handle.width+20, handle.height+20
+              app.resize handle.width+20, handle.height+20
+              r.style    :width => handle.width+2,  :height => handle.height+2
+
+              # Display new image.
+              img     = image(:data => handle).tap { |i| i.move(11, 11) }
+            rescue => e
+              alert "There appears to be an error in your plot code. Here's the trace:\n\n" + e.backtrace.join("\n"),
+                    :title => "Rubyvis Error - SciRuby"
+            end
+          end
+        end if update
+
       end
     end
 

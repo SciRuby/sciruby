f951008bcc074d59124e116156dec796fb6b58f3
Daniel Mendler <mail@daniel-mendler.de>
2015-06-11 19:18:59 +0200

    fix submodule autoloading

--- lib/sciruby/autoload.rb
+++ lib/sciruby/autoload.rb
@@ -2,12 +2,16 @@ class Module
   alias :const_missing_without_sciruby :const_missing
 
   def const_missing(name)
-    paths = SciRuby.autoload_modules.delete(name.to_sym)
-    if paths
-      paths.each {|path| require path }
-      const_get(name)
-    else
+    (Object != self && sciruby_autoload("#{self.name}::#{name}")) ||
+      sciruby_autoload(name) ||
       const_missing_without_sciruby(name)
-    end
+  end
+
+  private
+
+  def sciruby_autoload(name)
+    return unless paths = SciRuby.autoload_modules.delete(name.to_sym)
+    paths.each {|path| require path }
+    const_get(name)
   end
 end
